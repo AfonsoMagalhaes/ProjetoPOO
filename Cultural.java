c0aff1b63c162f2ce64eec3e38a00be337c4ccf1
AfonsoMagalhaes <45210366+AfonsoMagalhaes@users.noreply.github.com>
2018-12-02 18:22:39 +0000

    Delete Cultural.java

--- Cultural.java
+++
@@ -1,7 +0,0 @@
-package po;
-
-public class Cultural extends Parque {
-    public Cultural(String nome, String horario, float entrada, float custoextra){
-        super(nome, horario, entrada, custoextra);
-    }
-}
